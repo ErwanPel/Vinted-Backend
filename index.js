0aa20454d615f4c191e6f6b7b95eb4277893421d
Erwan <erwanpellerinpro@gmail.com>
2023-05-25 20:16:39 +0200

    add cors and sxript start

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
